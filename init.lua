c4898ee314ff7061f4094640f4d0f2159f0c18c1
Ruthenic <mdrakea3@tutanota.com>
2021-10-11 02:38:45 -0400

    use 4 width tabs

--- init.lua
+++ init.lua
@@ -26,6 +26,8 @@ set.mouse = 'a'
 set.clipboard = 'unnamedplus'
 set.pumheight =5
 set.number = true
+set.tabstop = 4
+set.shiftwidth = 4
 vim.cmd('colorscheme onedark') -- I would assume there would be a more lua-y way to do this... right?
 require("plugins")
 require("keybinds")
