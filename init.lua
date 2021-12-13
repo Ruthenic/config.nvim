a9edb74e11fc55667c062720f90a2e334e72c8f7
Ruthenic <mdrakea3@tutanota.com>
2021-12-12 19:15:49 -0500

    Move to paq
    Hopefully more speed because I hardly used packer's features, and paq is
    much smol

--- init.lua
+++ init.lua
@@ -1,28 +1,28 @@
 vim.g.did_load_filetypes = 1
 --TODO: switch to a more lua friendly plugin manager
-require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use "nathom/filetype.nvim"
-	use 'neovim/nvim-lspconfig'
-	use 'kyazdani42/nvim-web-devicons'
-	use 'nvim-lualine/lualine.nvim'
-	use 'nvim-lua/plenary.nvim'
-	use 'nvim-telescope/telescope.nvim'
-	use 'kyazdani42/nvim-tree.lua'
-	use 'lukas-reineke/indent-blankline.nvim'
-	use 'sainnhe/sonokai'
-	use 'yamatsum/nvim-cursorline'
-	use 'nvim-treesitter/nvim-treesitter'
-	use {'ms-jpq/coq_nvim', branch = 'coq'}
-	use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
-	use {'ms-jpq/coq.thirdparty', branch = '3p'}
-	use {'noib3/cokeline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
-	use 'numtostr/FTerm.nvim'
-	use 'andweeb/presence.nvim'
-	use 'tpope/vim-fugitive'
-	use 'folke/todo-comments.nvim'
-	use 'dstein64/vim-startuptime'
-end)
+require("paq")({
+	'wbthomason/packer.nvim';
+	"nathom/filetype.nvim";
+	'neovim/nvim-lspconfig';
+	'kyazdani42/nvim-web-devicons';
+	'nvim-lualine/lualine.nvim';
+	'nvim-lua/plenary.nvim';
+	'nvim-telescope/telescope.nvim';
+	'kyazdani42/nvim-tree.lua';
+	'lukas-reineke/indent-blankline.nvim';
+	'sainnhe/sonokai';
+	'yamatsum/nvim-cursorline';
+	'nvim-treesitter/nvim-treesitter';
+	{'ms-jpq/coq_nvim', branch='coq'};
+	{'ms-jpq/coq.artifacts', branch='artifacts'};
+	{'ms-jpq/coq.thirdparty', branch='3p'};
+	'noib3/cokeline.nvim';
+	'numtostr/FTerm.nvim';
+	'andweeb/presence.nvim';
+	'tpope/vim-fugitive';
+	'folke/todo-comments.nvim';
+	'dstein64/vim-startuptime';
+})
 require("colorscheme")
 require("config")
 require("plugins")
